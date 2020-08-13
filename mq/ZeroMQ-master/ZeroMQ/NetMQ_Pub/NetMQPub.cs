612e14a0c88ead24c9d2f3f7fc58f384ae81bb7e
yangzhongchu <455721762@qq.com>
2020-08-13 09:56:37 +0800

    服了都  这git拷了别人的仓库到自己的仓库 居然不能push！！！！

---
+++ mq/ZeroMQ-master/ZeroMQ/NetMQ_Pub/NetMQPub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using NetMQ;
+
+namespace NetMQ_Pub
+{
+    public class NetMQPub
+    {
+        readonly static ManualResetEvent _terminateEvent = new ManualResetEvent(false);
+        /// <summary>
+        /// NetMQ 发布端
+        /// </summary>
+        public static void Start()
+        {
+            string[] weathers = new string[6] { "晴朗", "多云", "阴天", "霾", "雨", "雪" };
+             
+            Console.WriteLine("发布多个地区天气预报：");
+
+            using (NetMQContext context = NetMQContext.Create())
+            {
+                using (var publisher = context.CreatePublisherSocket())
+                {
+                    publisher.Bind("tcp://127.0.0.1:5556");
+
+                    var rng = new Random();
+                    string msg;
+                    int sleeptime = 1000;//1秒
+
+                    ///指定发布的时间间隔，1秒
+                    while (_terminateEvent.WaitOne(1000) == false)
+                    {
+                        //随机生成天气数据
+                        int zipcode = rng.Next(0, 99);
+                        int temperature = rng.Next(-50, 50);
+                        int weatherId = rng.Next(0, 5);
+
+                        msg = string.Format("{0} {1} {2}", zipcode, temperature, weathers[weatherId]);
+                        publisher.SendFrame(msg);
+
+                        Console.WriteLine(msg);
+                        Thread.Sleep(sleeptime);
+                    }
+                }
+            }
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("exit……");
+            _terminateEvent.Set();
+        }
+    }
+}
---
+++ mq/ZeroMQ-master/ZeroMQ/OMQ_Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NetMQ;
+
+namespace OMQ_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NetMQContext context = NetMQContext.Create())
+            {
+                Server(context);
+            }
+        }
+
+        private static void Server(NetMQContext context)
+        {
+            using (NetMQSocket serverSocket = context.CreateResponseSocket())
+            {
+                serverSocket.Bind("tcp://127.0.0.1:5555");
+                while (true)
+                {
+                    string message1 = serverSocket.ReceiveFrameString();
+
+                    Console.WriteLine("Receive message :\r\n{0}\r\n", message1);
+
+                    string[] msg = message1.Split(':');
+                    string message = msg[1];
+
+
+                    #region 根据接收到的消息，返回不同的信息
+                    if (message == "Hello")
+                    {
+                        serverSocket.SendFrame("World");
+                    }
+                    else if (message == "ni hao ")
+                    {
+                        serverSocket.SendFrame("你好！");
+                    }
+                    else if (message == "hi")
+                    {
+                        serverSocket.SendFrame("HI");
+                    }
+                    else
+                    {
+                        serverSocket.SendFrame(message);
+                    }
+                    #endregion
+
+
+
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
